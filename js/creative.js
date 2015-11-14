55c145e841770ccbfa343c840c67f3766318c07c
Rachit Garg <rox.rachit@gmail.com>
2015-11-14 18:34:48 +0530

    added menu

--- js/creative.js
+++ js/creative.js
@@ -130,4 +130,28 @@
         }
     });
 
+
+    // JS to make the menu modal full screen
+    $(".modal-transparent").on('show.bs.modal', function () {
+      setTimeout( function() {
+        $(".modal-backdrop").addClass("modal-backdrop-transparent");
+      }, 0);
+    });
+    $(".modal-transparent").on('hidden.bs.modal', function () {
+      $(".modal-backdrop").addClass("modal-backdrop-transparent");
+    });
+
+    $(".modal-fullscreen").on('show.bs.modal', function () {
+      setTimeout( function() {
+        $(".modal-backdrop").addClass("modal-backdrop-fullscreen");
+      }, 0);
+    });
+    $(".modal-fullscreen").on('hidden.bs.modal', function () {
+      $(".modal-backdrop").addClass("modal-backdrop-fullscreen");
+    });
+
+    $('#carousel-example-generic').carousel({
+        interval: 300000
+    });
+
 })(jQuery); // End of use strict
