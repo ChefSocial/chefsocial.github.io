215c2a8f1f86077a9119215e749192ed61c57655
Rachit Garg <rox.rachit@gmail.com>
2016-05-30 21:10:15 +0530

    Added animation for step navigation + Added address form for step 2 for all the chefs

--- js/creative.js
+++ js/creative.js
@@ -105,7 +105,29 @@
     $('.close-menu-modal').on('click', function(){
         var chef = $(this).closest('.modal').attr('chef');
         var menu = $(this).attr('menu');
+        // Show step-1 and Hide the step 2 and 3 of the booking modal by default
+        $('#' + chef + 'BookingModal .step[step="1"]').show();
+        $('#' + chef + 'BookingModal .step[step="2"], #' + chef + 'BookingModal .step[step="3"]').hide();
+        // Assigning the src of the image dynamically
         $('#'+chef+'BookingModal .menu-image').attr("src", "img/menu/"+chef+"-booked-menu-"+menu+".jpg");
+    });
+
+    // Generic navigation when clicked on navigate buttom to take to corresponding step
+    $('.menu-booking-modal .navigate').on('click', function(){
+        // getting the action to got to next or previous
+        var action = $(this).attr("action");
+        var currentStep = $(this).closest('.step');
+        var currentStepNumber = currentStep.attr('step');
+        if(action == "next"){
+            var futureStepNumber = parseInt(currentStepNumber) + 1;
+        }else{
+            var futureStepNumber = parseInt(currentStepNumber) - 1;
+        }
+        // show future step with animation + hide current step with animation
+        var futureStep = currentStep.siblings(".step[step='" +  futureStepNumber + "']");
+        currentStep.fadeOut(250, function(){
+            futureStep.fadeIn(250);
+        });
     });
 
 })(jQuery); // End of use strict
